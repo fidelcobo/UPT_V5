164228f735218915819b915af71b4bc57a3f067e
fidelcobo <fidel.cobp@gmail.com>
2022-03-30 16:10:15 +0200

    Reescritas dos clases con @dataclass. No varía el ejecutable

--- aux_class/clase_auxiliar.py
+++ aux_class/clase_auxiliar.py
@@ -1,18 +1,20 @@
 from collections import namedtuple
+from dataclasses import dataclass
 
 Cisco_Articles = namedtuple('Cisco_Articles', 'sku serv_lev')
 DatosGenerales = namedtuple('DatosGenerales', 'nombre_oferta cliente bid version am')  # V4
 
 
+@dataclass()
 class CiscoItem:
+    sku: str
+    serv_lev: str
+    backout: str = ''
+    list_price: float = 0
+    eos: str = ''
+    smartnet_list: str = ''
 
-    def __init__(self, sku, serv_lev, backout='', list_price=0, eos='', smartnet_list=''):
-        self.sku = sku
-        self.serv_lev = serv_lev
-        self.backout = backout
-        self.list_price = list_price
-        self.eos = eos
-        self.smartnet_list = smartnet_list
+    def __post_init__(self):
         self.smartnet_sku = ''
         self.service_price_list = ''
         self.gdc_cost = 0.0
@@ -25,25 +27,25 @@ class CiscoItem:
                                                                                           self.smartnet_list))
 
 
+@dataclass()
 class ElementosOferta:
     """
     Esta clase se utilizará para manejar los elementos no repetidos de la quote, de forma que pueda hacerse una
     oferta económica resumida para el cliente. V4
     """
 
-    def __init__(self, in_csv, manufacturer, code, qty, init_date, end_date, uptime, total_price=0):
-        self.in_csv = in_csv
-        self.manufacturer = manufacturer
-        self.code = code
-        self.qty = qty
-        self.init_date = init_date
-        self.end_date = end_date
-        self.uptime = uptime
-        self.total_price = total_price
+    in_csv: str
+    manufacturer: str
+    code: str
+    qty: int
+    init_date: str
+    end_date: str
+    uptime: str
+    total_price: float = 0.0
 
 
 class FullElementosOferta(ElementosOferta):
-    
+
     def __init__(self, uptime_descr, tech, gpl, cost_backout, venta_backout, total_cost,
                  total_sell_price, backout_name, serial_no, duration, currency,
                  in_csv, manufacturer, code, qty, init_date, end_date, uptime, total_price):
